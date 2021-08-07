b3f703c8a056f9ac7b78f9370018e11fa507c4d5
shengjie <ysjshengjie@live.cn>
2021-08-07 13:18:21 +0800

    add vip migrations

--- src/EShopOnAbp.EntityFrameworkCore/Migrations/20210807042805_Add_Vip_AggregateRoot.Designer.cs
+++ src/EShopOnAbp.EntityFrameworkCore/Migrations/20210807042805_Add_Vip_AggregateRoot.Designer.cs
@@ -4,15 +4,17 @@ using EShopOnAbp.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 
 namespace EShopOnAbp.Migrations
 {
     [DbContext(typeof(EShopOnAbpDbContext))]
-    partial class EShopOnAbpDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210807042805_Add_Vip_AggregateRoot")]
+    partial class Add_Vip_AggregateRoot
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/EShopOnAbp.EntityFrameworkCore/Migrations/20210807042805_Add_Vip_AggregateRoot.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EShopOnAbp.Migrations
+{
+    public partial class Add_Vip_AggregateRoot : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(36)", maxLength: 36, nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NicName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ExtraProperties = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ConcurrencyStamp = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vips",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(36)", maxLength: 36, nullable: false),
+                    CustomerId = table.Column<string>(type: "nvarchar(36)",maxLength: 36, nullable: false),
+                    Score = table.Column<int>(type: "int", nullable: false),
+                    ExtraProperties = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ConcurrencyStamp = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vips", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VipScoreRecords",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    VipId = table.Column<string>(type: "nvarchar(36)", nullable: false),
+                    RecordType = table.Column<string>(type: "nvarchar(16)", maxLength: 16, nullable: false),
+                    RecordStatus = table.Column<string>(type: "nvarchar(16)", maxLength: 16, nullable: false),
+                    Before = table.Column<int>(type: "int", nullable: false),
+                    Changed = table.Column<int>(type: "int", nullable: false),
+                    After = table.Column<int>(type: "int", nullable: false),
+                    Left = table.Column<int>(type: "int", nullable: false),
+                    RecordDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastUpdateDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VipScoreRecords", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_VipScoreRecords_Vips_VipId",
+                        column: x => x.VipId,
+                        principalTable: "Vips",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vips_CustomerId",
+                table: "Vips",
+                column: "CustomerId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VipScoreRecords_VipId",
+                table: "VipScoreRecords",
+                column: "VipId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "VipScoreRecords");
+
+            migrationBuilder.DropTable(
+                name: "Vips");
+        }
+    }
+}
